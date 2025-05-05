9e13b9d0efbc51e7294c71345c3a5f3d51e89b6a
Piyush Kumar <piyushkumar07999@gmail.com>
2025-05-05 17:24:53 +0530

    adding:login schema

--- auth.ts
+++ auth.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import Google from "next-auth/providers/google"
- 
+import { prisma } from "@/lib/prisma"
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google,GitHub],
+  adapter: PrismaAdapter(prisma),
+  providers: [Google],
 })
